c349b62f0b3adf468fcc3dce1110f3309974f3cd
원자로개구리 <jhjjgu0115@naver.com>
2017-12-12 00:23:03 +0900

    씬매니저 생성

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public partial class Unit : MonoBehaviour
 {
-    
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Coordinate")]
+    [SerializeField]
+    int x;
+    [SerializeField]
+    int y;
 
-    void HunJaeCode()
-    { 
-        Debug.Log("훈재가 씀");
+    public void Move(int x,int y)
+    {
+        Tile movePositionTile = TileMapManager.Instance.GetTile(x, y);
+        if(movePositionTile)
+        {
+            Debug.Log("CanMove (" + x + "," + y + ")");
+        }
+        else
+        {
+            transform.position = movePositionTile.transform.position;
+        }
     }
+    public void MoveUp()
+    {
+
+    }
+    public void MoveDown()
+    {
+
+    }
+    public void MoveLeft()
+    {
+
+    }
+    public void MoveRight()
+    {
+
+    }
+
+
+
+    void Start ()
+    {
+	}
+	void Update ()
+    {
+        // w
+        //asd
+	}
 }
 public partial class Unit : MonoBehaviour
 {
